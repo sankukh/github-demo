6e0b836c669d107c42cf51e43b92199f288cb6ae
sankukh <sankumisme@gmail.com>
2021-05-04 14:36:37 +0530

    Update Demo.java

--- src/com/sanket/githubdemo/Demo.java
+++ src/com/sanket/githubdemo/Demo.java
@@ -1,11 +1,11 @@
 package com.sanket.githubdemo;
+import java.utils.Date;
 
 public class Demo {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World");
-
+		System.out.println("Today's Date :: "+new Date());
 	}
 
 }
