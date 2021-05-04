079c12f45a4ba8d51368a9ab4ca7db334bb0f30f
sankukh <sankumisme@gmail.com>
2021-05-04 13:59:10 +0530

    Update Demo.java

--- src/com/sanket/githubdemo/Demo.java
+++ src/com/sanket/githubdemo/Demo.java
@@ -3,8 +3,7 @@ package com.sanket.githubdemo;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Hello I am the Main class");
-		System.out.println("I am Just Printing Hello World!");
+		System.out.println("Hello World");
 
 	}
 
